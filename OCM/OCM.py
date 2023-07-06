9b9eb9ef414ad030bddc64cf2ed286d34cd76bd9
Philip Otter <78989650+Philip-Otter@users.noreply.github.com>
2023-07-06 15:39:25 -0800

    Update to allow modular header support

--- OCM/OCM.py
+++ OCM/OCM.py
@@ -26,14 +26,26 @@ def loadHTML():
     return(html)
 
 
+def headers(self):
+    self.send_response(200)
+    self.send_header("Content-type", "text/html")
+    self.end_headers()
+
+
 def startServer(port):
     class handler(BaseHTTPRequestHandler):
+        
+        
         def do_GET(self):
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-
+            headers(self)
             self.wfile.write(bytes(loadHTML(), "utf-8"))
+
+
+        def do_POST(self):
+            if(self.path == "/mainpage/load"):
+                headers(self)
+                self.wfile.write(bytes("load API", "utf-8"))
+
 
     server = HTTPServer(("localhost",port), handler)
     server.serve_forever()
