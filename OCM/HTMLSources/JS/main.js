67405d145065e81c76458845feb67360feb4f8f9
Philip Otter <78989650+Philip-Otter@users.noreply.github.com>
2023-07-06 15:38:45 -0800

    Add javascript interaction for main page

---
+++ OCM/HTMLSources/JS/main.js
@@ -0,0 +1,9 @@
+//  ||======================================||
+//  || Copyright 2023 Philip Otter          ||
+//  ||                                      ||
+//  || Handles the main page interactions   ||
+//  ||======================================||
+
+
+// Clicking the load button
+const  loadButton = document.getElementById("myBtn");
