43a64be1a930d01a1f0917830a722bc63b2f342e
Philip Otter <78989650+Philip-Otter@users.noreply.github.com>
2023-07-10 15:01:01 -0800

    Minor updates

--- OCM/Interpreter.py
+++ OCM/Interpreter.py
@@ -1,43 +1,72 @@
 ## Copyright 2023 Philip Otter
 
 
-def cheat_line(line, architecture, isTargetSet):
+def cheat_line(line, architecture, isTargetSet: bool, path):
     print("Cheat Line Function")
 
+    # Split our line values
+    lineFace = line.split('|')[0]  # InteractionType:CheatName
+    lineBody = (line.split('|')[1]).split('>')[0]  # BaseOffset:PointerOffsets
+    lineTail = (line.split('|')[1]).split('>')[1]  # if isTargetSet=False:  ModValue<TargetModule  # If isTargetSet=True:  ModValue
+    # Further break down of our values
+    interactionType = lineFace.split(":")[0]
+    cheatName = lineFace.split(":")[1]
+    baseOffset = lineBody.split(":")[0]
+    pointerOffset = lineBody.split(":")[1]
 
-def mod_name(name):
+    if(isTargetSet):
+        ModValue = lineTail
+    else:
+        ModValue = lineTail.split("<")[0]
+        targetModule =  lineTail.split("<")[1]
+    
+
+
+
+    def python_64():
+        print("Python 64bit function")
+
+
+    def python_32():
+        print("Python 32bit function")
+
+    # Evaluate our architecture
+    
+
+
+def mod_name(name, path):
     print("Mod Name Function")
 
 
-def program_version(line):
+def program_version(line, path):
     print("Program Version Function")
 
 
-def target_module(line):
+def target_module(line, path):
     print("Target Module Function")
 
 
-def set_theme(theme):
+def set_theme(theme, path):
     print("Set Theme Function")
 
 
-def set_application(app):
+def set_application(app, path):
     print("Set Application Function")
 
 
-def gen_HTML(HTMLLine):
+def gen_HTML(HTMLLine, path):
     print("Gen HTML Function")
 
 
-def gen_JavaScript(JSLine):
+def gen_JavaScript(JSLine, path):
     print("Gen JavaScript Function")
 
 
-def gen_Python_3(PythonLine):
+def gen_Python_3(PythonLine, path):
     print("Gen Python 3 Function")
 
 
-def read_File(file):
+def read_File(file, path):
     with open(file,'r') as oyster:
 
         modderName = None
@@ -49,24 +78,24 @@ def read_File(file):
         print("Checking for {V}, {T}, {C}, {X}")
         for line in oyster:
             if('{V}' in line):
-                program_version(line.strip('{V}'))
+                program_version(line.strip('{V}'), path)
             elif('{T}' in line):
-                target_module(line.strip('{T}'))
+                target_module(line.strip('{T}'), path)
                 targetSet = True
             elif('{C}' in line):
                 theme = line.strip('{C}')
             elif('{X}' in line):
                 arch = line.strip('{X}')
             elif('{APP}' in line):
-                set_application(line.strip('{APP}'))
+                set_application(line.strip('{APP}'), path)
             else:
                 pass
 
             # Checks to see if default theme needs to be used.
             if(theme != None):
-                set_theme(theme)
+                set_theme(theme, path)
             else:
-                set_theme("Default")
+                set_theme("Default", path)
 
             # Checks to make sure that the modder provided the architecture
             if(arch != None):
@@ -82,26 +111,18 @@ def read_File(file):
             if('{@}' in line):
                 modderName = line.strip('{@}')
             elif('{}' in line):
-                cheat_line(line.strip('{}'),arch, targetSet)
+                cheat_line(line.strip('{}'),arch, targetSet, path)
             elif('{H5}' in line):
-                gen_HTML(line.strip('{H5}'))
+                gen_HTML(line.strip('{H5}'), path)
             elif('{JS}' in line):
-                gen_JavaScript(line.strip('{JS}'))
+                gen_JavaScript(line.strip('{JS}'), path)
             elif('{P3}' in line):
-                gen_Python_3(line.strip('{P3}'))
+                gen_Python_3(line.strip('{P3}'), path)
             else:
                 pass
 
         # Checks to see if the modder provided their name. 
         if(modderName != None):
-                    mod_name(modderName)
+                    mod_name(modderName, path)
         
         oyster.close()
-
-
-
-
-
-
-                
-
